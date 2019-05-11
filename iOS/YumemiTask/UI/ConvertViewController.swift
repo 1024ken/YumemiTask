c633677a1289d833f0999b4aa54ff5fc2a1bc9f0
渡辺健一 <watanabe-kenichi@rouge.plala.or.jp>
2019-05-11 17:29:17 +0900

    メインUI・ConvertViewController作成

---
+++ iOS/YumemiTask/UI/ConvertViewController.swift
@@ -0,0 +1,22 @@
+//
+//  ConvertViewController.swift
+//  YumemiTask
+//
+//  Created by 渡辺健一 on 2019/05/11.
+//  Copyright © 2019 渡辺健一. All rights reserved.
+//
+
+import UIKit
+
+class ConvertViewController: UIViewController {
+
+    @IBOutlet weak var convertLabel: UILabel!
+    @IBOutlet weak var inputTextField: UITextField!
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        
+        
+    }
+
+}
